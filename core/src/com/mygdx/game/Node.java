0aefcc3da8dba84364aba64e998a887f9a73fe7b
Ben Chadler <benchanwaal@yahoo.co.uk>
2018-11-25 19:23:04 +0000

    New Node w. ability to make map of nodes

--- core/src/com/mygdx/game/Node.java
+++ core/src/com/mygdx/game/Node.java
@@ -1,22 +1,13 @@
-//id                int
-//connected nodes   list
-//nodeType          String
-//weather           String
-//Score             Integer
-//Visibility        Boolean
-//encounterChance   list
-//x                 int
-//y                 int
-//giveEncounter()   encounter
-//getCoord()        List<integer>
-//applyEffect()     Void
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
+//import java.util.Random;
+//import java.util.function.IntToDoubleFunction;
+//import java.util.Arrays;
+//import java.util.Comparator;
 
 
 public class Node {
     private int id;                 //Unique node identifier
-    private int[] conectnodes;      //Other nodes that are attached
+    private ArrayList<Integer> connectnodes;      //Other nodes that are attached to the current node
     private String nodeType;        //Type of the node
     private String weather;         //What weather the node has
     private boolean visibility;     //What the visibility is?
@@ -24,54 +15,138 @@ public class Node {
     private int encounterchance;    //Chance of an encounter
     private int x;                  //x location of node
     private int y;                  //y location of node
-}
 
-public Node(int id, int x, int y){
-    this.id = id;
-    this.x = x;
-    this.y = y;
-}
+    //Instatiates the Node
+    public Node(int id, int x, int y){
+        this.id = id;
+        this.x = x;
+        this.y = y;
+    }
 
 
-public void applyEffect();          //What effect the node has
+//public void applyEffect();          //What effect the node has
 
     //Gets co-ordinates of the node
-public String[] getCoord(){
-    String xy[] = {x,y};
-    return xy;
-}
-
-//Randomly decides on which encounter to selectz
-public void giveEncounter() {
-    Random chance = new Random();
-    int n = chance.nextInt(100)+1;
-    if (n > encounterchance) {
-        chance = new Random();
-        int n = chance.nextInt(encounters.size());
-        //encounter[n]
-    } else {
-        //no encounter
+    public Integer[] getCoord(){
+        Integer xy[] = {x,y};
+        return xy;
     }
-}
 
-public void nodeMapGenerator(){
-    Node[] Map = new Node[20];
-    Random randomx = new Random();
-    Random randomy = new Random();
-    for (int i = 0; i < Map.length(); i++){
-        
+    //Randomly decides on which encounter to selectz
+    public void giveEncounter() {
+        Random chance = new Random();
+        int n = chance.nextInt(100)+1;
+        if (n > encounterchance) {
+            chance = new Random();
+            n = chance.nextInt(encounters.length);
+            //encounter[n]
+        } else {
+            //no encounter
+        }
     }
+
     //Generate list of nodes
     //each node has (initially) id, x, y - add everything else later
     //id = 0,1,2...
     //x+y = random, regenerate if within z radius of other nodes
-    //or generate nodes recursively, each node is within a to b distance from each previous node
-    //can only generate front 270 degrees?
     //Attach each node to 3 nearest nodes
-    //distance from each node to another = sqrt((x2-x1)*2 + (y2-y1)*2)
 
-}
-//Sets encounterchance
+    public static Node[] nodeMapGenerator(){
+        //Defines the Map, and gets random numbers for the node location
+        Node[] Map = new Node[20];
+        Random randomx = new Random();
+        Random randomy = new Random();
+        Boolean inbounds = true;
+        for (int i = 0; i < Map.length; i++){
+            //Gives each node an id, and a default location
+            Map[i] = new Node(i,0,0);
+            inbounds = true;
+            //Loop to check no node is within x distance of each other
+            while(inbounds == true){
+                inbounds = false;
+                //Random co-ordinates generating
+                Map[i].x = randomx.nextInt(200);
+                Map[i].y = randomx.nextInt(200);
+
+                //Check to see whether the co-ordinates are within x of another node, if yes loop
+                for (int j = 0; j < i; j++){
+                    if (Math.sqrt((Math.pow((Map[i].x - Map[j].x),2) + Math.pow((Map[i].y - Map[j].y),2))) < 30){
+                        inbounds = true;
+                    }
+                }
+
+                randomx = new Random();
+                randomy = new Random();
+            }
+
+        }
+
+        //Stores distance and id of each node
+        //Finds 3 closest nodes to every node, and adds their ids to connectnodes
+        Double[][] Distance = new Double[20][2];
+        for (int i = 0; i < Map.length; i++){
+            for (int j = 0; j < Map.length; j++){
+                //Stores id of node at j1, distance at j2
+                Distance[j][1] = Double.valueOf(j);
+                Distance[j][0] = Math.sqrt((Math.pow((Map[i].x - Map[j].x),2) + Math.pow((Map[i].y - Map[j].y),2)));
+                //Makes sure nodes are never connected to themselves, because of distance
+                if (i == j){
+                    Distance[j][0] = 9999.9;    //Never in bound
+                }
+            }
+
+            //Sort node distance
+            Arrays.sort(Distance, new Comparator<Double[]>() {
+                @Override
+                public int compare(final Double[] entry1, final Double[] entry2) {
+                    final Double column1 = entry1[0];
+                    final Double column2 = entry2[0];
+                    if (column1 > column2){
+                        return 1;
+                    } else if (column1 == column2){
+                        return 0;
+                    } else {
+                        return -1;
+                    }
+                }
+            });
+            //Adds to the nearest 3 nodes to connectnodes
+            Map[i].connectnodes = new ArrayList<Integer>();
+            for (int k = 0; k < 3; k++){
+                Map[i].connectnodes.add(Distance[k][1].intValue());
+            }
+
+
+        }
+
+        //Make all nodes are connected both ways
+        for (int i = 0; i < Map.length; i++){
+            for (int j = 0; j < Map.length; j++){
+                //Does another node contain the current nodes id, but isn't recorded
+                if (Map[j].connectnodes.contains(Map[i].id) && (!Map[i].connectnodes.contains(Map[j].id))){
+                    Map[i].connectnodes.add(Map[j].id);
+                }
+            }
+
+        }
+
+        //Test printing
+//        for (int i = 0; i < Map.length; i++){
+//            for (int j = 0; j < Map.length; j++){
+//                System.out.print(Distance[j][0]);
+//                System.out.println(" " + Distance[j][1]);
+//            }
+//            System.out.println(Map[i].connectnodes);
+//            System.out.println("NEW ARRAY");
+//        }
+
+        //Returns completed map of nodes
+        return Map;
+    }
+
+
+
+    //Sets encounterchance
     public void setEncounterchance(int encounterchance){
         this.encounterchance = encounterchance;
     }
@@ -101,5 +176,26 @@ public void nodeMapGenerator(){
         return weather;
     }
 
-    //id, x, y
+    //Returns x value
+    public Integer getX(){
+        return x;
+    }
+
+    //Returns y value
+    public Integer getY(){
+        return y;
+    }
+
+    //Returns ID value
+    public Integer getId(){
+        return id;
+    }
+
+    //Returns all nodes connected to current node
+    public ArrayList<Integer> getConnectnodes(){
+        return this.connectnodes;
+    }
+
+//ArrayList<Integer> list=new ArrayList<Integer>();
+}
 
