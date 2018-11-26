4767007559dccaa9eda41b7515bf91fac6ba73d6
Jordan <jll541@york.ac.uk>
2018-11-26 15:48:00 +0000

    test

--- core/src/com/mygdx/game/Card.java
+++ core/src/com/mygdx/game/Card.java
@@ -1,5 +1,5 @@
 package com.mygdx.game;
-import java.util.*
+import java.util.*;
 
 public class Card {
     private Integer cardID; //ID integer for each card
@@ -21,6 +21,59 @@ public class Card {
         numberOfCards++;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
 
+    }
+
+    public void setCardID(Integer cardID) {
+        this.cardID = cardID;
+    }
+
+    public Integer getCardID() {
+        return cardID;
+    }
+
+    public void setDescription (String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setShopCost(Integer shopCost) {
+        this.shopCost = shopCost;
+    }
+
+    public Integer getShopCost() {
+        return shopCost;
+    }
+    public void setGoldCost(Integer goldCost) {
+        this.goldCost = goldCost;
+    }
+
+    public Integer getGoldCost() {
+        return goldCost;
+    }
+
+    public void setManaCost(Integer manaCost) {
+        this.manaCost = manaCost;
+    }
+
+    public Integer getManaCost() {
+        return manaCost;
+    }
+
+    public void setEffect (String effect) {
+        this.effect = effect;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
 }
