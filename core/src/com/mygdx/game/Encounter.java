b4922ac673689a043acf6d2796259361d1e09e70
Unknown <Patrick.maloney123@yahoo.co.uk>
2018-11-26 16:48:47 +0000

    Adding code to Encounter
    Don't know why this didn't go up with the class

--- core/src/com/mygdx/game/Encounter.java
+++ core/src/com/mygdx/game/Encounter.java
@@ -1,4 +1,51 @@
 package com.mygdx.game;
 
+
+import java.util.ArrayList;
+
+/*
+Not actually annotations: trying to make notes of how this should work
+An encounter occuring is an instance of the encounter object
+When an instance is created, a type of encounter is randomly chosen
+For that type of encounter: options, score and descriptions are read from a database and effects is entered at initialisation
+
+*/
 public class Encounter {
+    private ArrayList<String> options;
+    private ArrayList<String> effects;
+    private int score;
+    private String description;
+
+    public Encounter(){
+        Encounter encounter = new Encounter();
+        encounter.effects = new ArrayList<String>();
+        /*
+        TO DO:
+        Type of encounter is randomly chosen and options, effects and description are all read from some kind of database
+
+         */
+
+    }
+
+    public Encounter(ArrayList<String> effects){
+        Encounter encounter = new Encounter();
+        encounter.effects = effects;
+        /*
+        TO DO:
+        Type of encounter is randomly chosen and options, effects and description are all read from some kind of database
+
+         */
+
+    }
+
+    public void chooseOption(int choice){
+
+    }
+
+    /*
+    NOTE: Battle doesn't exist so this code throws up an error
+    public Battle startBattle(Ship playerShip, Ship enemyShip){
+        return
+    }
+    */
 }
